daf3968ef3057d6d53bfe2de232ce2b3dd181105
Darly Guerrero <dlguerre@espol.edu.ec>
2017-11-18 20:09:07 -0500

    Creacion de la clase Miembros y HechiceroPlanificador

---
+++ src/proyectodepoo1par/HechiceroPlanificador.java
@@ -0,0 +1,13 @@
+package proyectodepoo1par;
+
+public class HechiceroPlanificador extends Rol {
+
+    public HechiceroPlanificador(String usuario, String contraseña, String nombre, String apellido, String rol) {
+        super(usuario, contraseña, nombre, apellido, rol);
+    }
+
+    @Override
+    public void TipoMago() {
+        //Animagos, Metamorfomago y Normal
+    }
+}
---
+++ src/proyectodepoo1par/Miembros.java
@@ -0,0 +1,45 @@
+package proyectodepoo1par;
+
+public class Miembros extends Rol {
+
+    private int edad;
+    private String casa;
+    private String varita;
+
+    public Miembros(String usuario, String contraseña, String nombre, String apellido, String rol, int edad, String casa, String varita) {
+        super(usuario, contraseña, nombre, apellido, rol);
+        this.edad = edad;
+        this.casa = casa;
+        this.varita = varita;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setCasa(String casa) {
+        this.casa = casa;
+    }
+
+    public void setVarita(String varita) {
+        this.varita = varita;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getCasa() {
+        return casa;
+    }
+
+    public String getVarita() {
+        return varita;
+    }
+
+    @Override
+    public void TipoMago() {
+        //Animagos, Metamorfomago y Normal
+    }
+
+}
