0013d486b5f7eb542c7be355124689910eb4b10b
Darly Guerrero <dlguerre@espol.edu.ec>
2017-11-18 23:55:58 -0500

    Creacion de la clase enum Materias

---
+++ src/proyectodepoo1par/Materias.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectodepoo1par;
+
+/**
+ *
+ * @author Fastline
+ */
+public enum Materias {
+    POCIONES,
+    DefensaContraLasArtesOscuras,
+    ADIVINACIÓN,
+    ASTRONOMÍA,
+    HistoriaDeLaMagia,
+    HERBOLOGÍA,
+    ENCANTAMIENTOS,
+    VUELO
+}
