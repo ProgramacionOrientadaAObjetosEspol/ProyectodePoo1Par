14d9f21e67adc42b59cf067a8efb36327d8b1d00
Steven Araujo <saraujo@espol.edu.ec>
2017-11-26 01:43:13 -0500

    Comienzo de RolEstudiantes

--- src/proyectodepoo1par/Principal.java
+++ src/proyectodepoo1par/Principal.java
@@ -1,5 +1,6 @@
 package proyectodepoo1par;
 
+import OpcEstudiante.RolEstudiantes;
 import OpcPlanificador.RolPlanificador;
 import java.io.File;
 import java.util.ArrayList;
@@ -8,13 +9,14 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args){
         Scanner t = new Scanner(System.in);
-        ArrayList<ArrayList<String>> lineas = new ArrayList<>();
-        
         
         //objetos
+        ArrayList<ArrayList<String>> lineas = new ArrayList<>();
         ReadWriter archivo = new ReadWriter();
-        RolPlanificador planificador = new RolPlanificador();
+        RolPlanificador planificador = null;
+        RolEstudiantes estudiante = null;
         
+        //Variables
         String usuario,contraseña;
         
         System.out.println("        _________\n" +
@@ -51,12 +53,16 @@ public class Principal {
             if (validacion_ingreso == 1){
                 System.out.println("Ingreso exitoso\n");
                 if(validacion.equals("planificador")){
+                    planificador = new RolPlanificador();
                     System.out.println("1. Crear Curso \n2. Crear Profesor \n3. Crear Estudiante \n4. Ver Horarios Planificados \n5. Listado de estudiantes ");
                     opcion = t.nextInt();
                     t.nextLine();
                     if(opcion == 1){planificador.crearCurso();}
                 }else{
-                    System.out.println("estudiante");
+                    estudiante = new RolEstudiantes();
+                    System.out.println("1. Ver cursos planificados\n2. Registro\n3. Descripcion de vuelo\n ");
+                    opcion = t.nextInt();
+                    if(opcion == 1){estudiante.crearCurso();}
                 }
             }
             else{System.out.println("El usuario o la contraseña no son válidos. Por favor intente otra vez.");break;}
